2babfcbd1aab87645494a114347ae1b13675977f
Garry Laly <garrylaly@gmail.com>
2017-03-18 20:43:04 +0700

    gulp concat and minify css

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
 
 gulp.task('sass', function() {
    gulp.src('./sass/main.sass')
       .pipe(sass())
       .pipe(cleanCSS({compatibility: 'ie8'}))
       .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('compress', function() {
+  gulp.src([
+        'vendors/bootstrap/css/bootstrap.min.css',
+        'vendors/font-awesome/css/font-awesome.min.css',
+        'vendors/slick/slick.css',
+        'vendors/slick/slick-theme.css',
+        'css/main.css',
+      ])
+    .pipe(concat('build.css'))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./css/'));
 });
 
 gulp.task('automate', function() {
